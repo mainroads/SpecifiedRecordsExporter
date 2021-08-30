8a1b7d262e42a60ddf065ad9f7f1cb06180fcae4
Michael Delpach <McoreD@users.noreply.github.com>
2021-08-30 21:00:46 +0800

    Copy files ack checkbox

--- FolderStructureKiller/MainWindow.xaml.cs
+++ FolderStructureKiller/MainWindow.xaml.cs
@@ -14,28 +14,40 @@ namespace SpecifiedRecordsExporter
 
         private void btnBrowse_Click(object sender, RoutedEventArgs e)
         {
-            FolderBrowserForWPF.Dialog dlg = new FolderBrowserForWPF.Dialog();
-            dlg.Title = "Browse for the Specified Records folder...";
-
-            if (dlg.ShowDialog() == true)
+            if (chkCopyFiles.IsChecked == true)
             {
-                string dir = dlg.FileName;
-                if (Directory.GetParent(dir).Name == "Downloads")
+                FolderBrowserForWPF.Dialog dlg = new FolderBrowserForWPF.Dialog();
+                dlg.Title = "Browse for the Specified Records folder...";
+
+                if (dlg.ShowDialog() == true)
                 {
-                    txtRootDir.Text = dlg.FileName;
-                }
-                else
-                {
-                    MessageBox.Show("Specified Records subfolder is not in your Downloads folder!", Application.Current.MainWindow.Name);
+                    string dir = dlg.FileName;
+                    if (Directory.GetParent(dir).Name == "Downloads")
+                    {
+                        txtRootDir.Text = dlg.FileName;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Specified Records subfolder is not in your Downloads folder!", Application.Current.MainWindow.Title);
+                    }
                 }
             }
+            else
+            {
+                MessageBox.Show("You have not completed Step 1 above!", Application.Current.MainWindow.Title);
+            }
+
         }
 
         private async void btnGo_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(txtFreeText.Text))
             {
-                MessageBox.Show("Free Text is empty!", Application.Current.MainWindow.Name);
+                MessageBox.Show("Free Text is empty!", Application.Current.MainWindow.Title);
+            }
+            else if (chkCopyFiles.IsChecked == true)
+            {
+                MessageBox.Show("You have not completed Step 1 above!", Application.Current.MainWindow.Title);
             }
             else
             {
