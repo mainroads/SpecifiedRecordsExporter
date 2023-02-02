44090c06780ee874ebc43f50158ca4a80bb02656
Michael Delpach <McoreD@users.noreply.github.com>
2023-02-02 20:57:29 +0800

    Restructure for MVVM

---
+++ SpecifiedRecordsExporter/MVVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SpecifiedRecordsExporter
+{
+    public class MainPageViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        public new event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string RootDir => $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}{Path.DirectorySeparatorChar}Downloads{Path.DirectorySeparatorChar}Specified Records";
+        public string FreeText => "";
+
+        private ObservableCollection<string> filesColl = new ObservableCollection<string>();
+        public ObservableCollection<string> FilesCollection
+        {
+            get { return filesColl; }
+            set
+            {
+                if (value != this.filesColl)
+                    filesColl = value;
+                this.SetPropertyChanged("FileObjectCollection");
+            }
+        }
+
+        public MainPageViewModel()
+        {
+        }
+    }
+}
+
---
+++ SpecifiedRecordsExporter/MVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SpecifiedRecordsExporter
+{
+	public abstract class ViewModelBase
+	{
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
+
