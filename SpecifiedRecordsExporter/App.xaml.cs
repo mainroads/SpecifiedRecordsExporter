03a0e3c9c01707aab7fd0c62df63bdcc1c395e58
Michael Delpach <McoreD@users.noreply.github.com>
2023-02-08 21:04:54 +0800

    CreateWindow method to add title

--- SpecifiedRecordsExporter/App.xaml.cs
+++ SpecifiedRecordsExporter/App.xaml.cs
@@ -11,5 +11,15 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        if(window != null)
+        {
+            window.Title = Title;
+        }
+        return window;
+    }
 }
 
